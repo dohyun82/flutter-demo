b407b124c45c613f11cf27eb6d88e5535d7afbd8
김도현 <>
2022-12-19 13:27:02 +0900

    feat : Handling gestures

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,38 @@ void main() {
     /// Material 위젯을 사용하기 위해 MaterialApp 으로 실행한다.
     const MaterialApp(
       title: 'My app',
-      home: TutorialHome(),
+      home: Scaffold(
+        body: Center(
+          child: MyButton(),
+        ),
+      ),
     ),
   );
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('MyButton was tapped!');
+      },
+      child: Container(
+        height: 50.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: const Center(
+          child: Text('Engage'),
+        ),
+      ),
+    );
+  }
 }
 
 class TutorialHome extends StatelessWidget {
