6489c473ae576f27da12713a4ea7fd12438af5eb
김도현 <>
2022-12-19 13:50:58 +0900

    feat : Changing widgets in response to input

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,68 @@ void main() {
       title: 'My app',
       home: Scaffold(
         body: Center(
-          child: MyButton(),
+          child: Counter(),
         ),
       ),
     ),
   );
+}
+
+class CounterDisplay extends StatelessWidget {
+  const CounterDisplay({Key? key, required this.count}) : super(key: key);
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Count: $count');
+  }
+}
+
+class CounterIncrementer extends StatelessWidget {
+  const CounterIncrementer({Key? key, required this.onPressed})
+      : super(key: key);
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: const Text('Increment'),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  const Counter({Key? key}) : super(key: key);
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      ++_counter;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CounterIncrementer(onPressed: _increment),
+        const SizedBox(
+          width: 16,
+        ),
+        CounterDisplay(count: _counter),
+      ],
+    );
+  }
 }
 
 class MyButton extends StatelessWidget {
