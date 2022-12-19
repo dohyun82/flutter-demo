d387f9b30381d2f85c840f132c55dc595fa2c7c8
김도현 <>
2022-12-19 18:26:25 +0900

    feat : Tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -10,22 +10,130 @@ import 'package:flutter/material.dart';
 ///
 void main() {
   /// runApp 함수는 주어진 위젯을 가져와서 루트로 만든다.
-  runApp(
-    /// Material 위젯을 사용하기 위해 MaterialApp 으로 실행한다.
-    const MaterialApp(
-      title: 'My app',
-      home: Scaffold(
-        body: Center(
-            child: ShoppingList(
-          products: [
-            Product(name: 'Eggs'),
-            Product(name: 'Flour'),
-            Product(name: 'Chocolate chips'),
-          ],
-        )),
+  runApp(const MyApp());
+  // runApp(
+  //   /// Material 위젯을 사용하기 위해 MaterialApp 으로 실행한다.
+  //   const MaterialApp(
+  //     title: 'My app',
+  //     home: Scaffold(
+  //       body: Center(
+  //           child: ShoppingList(
+  //         products: [
+  //           Product(name: 'Eggs'),
+  //           Product(name: 'Flour'),
+  //           Product(name: 'Chocolate chips'),
+  //         ],
+  //       )),
+  //     ),
+  //   ),
+  // );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    "한강",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  '서울',
+                  style: TextStyle(color: Colors.grey[500]),
+                )
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
       ),
-    ),
-  );
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      title: "Flutter layout demo",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/lake.jpeg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        )
+      ],
+    );
+  }
 }
 
 class Product {
