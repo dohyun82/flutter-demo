ed0700ae7036117be5afb9710fefcabdb30d52c8
김도현 <>
2022-12-19 11:14:03 +0900

    feat : Basic widgets

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,70 @@ import 'package:flutter/material.dart';
 ///
 void main() {
   /// runApp 함수는 주어진 위젯을 가져와서 루트로 만든다.
-  runApp(const Center(
-    child: Text(
-      "Hello, world",
-      textDirection: TextDirection.ltr,
+  runApp(
+    /// Material 위젯을 사용하기 위해 MaterialApp 으로 실행한다.
+    const MaterialApp(
+      title: 'My app',
+      home: SafeArea(
+        child: MyScaffold(),
+      ),
     ),
-  ));
+  );
+}
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({Key? key, required this.title}) : super(key: key);
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      child: Row(
+        children: [
+          const IconButton(
+            onPressed: null,
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+          ),
+          Expanded(
+            child: title,
+          ),
+          const IconButton(
+            onPressed: null,
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: [
+          MyAppBar(
+            title: Text(
+              'Example Title',
+              style: Theme.of(context).primaryTextTheme.headline6,
+            ),
+          ),
+          const Expanded(
+            child: Center(
+              child: Text('Hello, world!'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
