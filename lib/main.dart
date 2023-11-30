8e91727ac5b51da363a6e18c0ed61ce7c282f766
김도현 <>
2023-11-30 09:20:52 +0900

    feat : 이미지 Crop 패키지 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/ui/ImagePickerDemo.dart';
+import 'package:flutter_demo/ui/ImageCropperDemo.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: ImagePickerDemoApp(),
+      home: MyImageCropperDemoApp(),
     ),
   );
 }
