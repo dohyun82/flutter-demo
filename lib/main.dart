d4e397c43ccf9e17d00c991c3c9e707353a84f03
김도현 <>
2024-01-12 16:04:10 +0900

    feat : List & Grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/ui/BuildLayout.dart';
+import 'package:flutter_demo/ui/ListsAndGrids.dart';
 
-void main() => runApp(const BuildLayoutApp());
+void main() {
+  runApp(MyLongList(
+    items: List<String>.generate(10000, (index) => 'Item $index'),
+  ));
+}
